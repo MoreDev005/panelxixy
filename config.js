5ea01bd4125e7f41cad74fef5cf4cd4265a7a9b9
Rurizora <bottoramku@gmail.com>
2026-02-25 00:40:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     admin: "iwan",
     password: "sandiku05",
     tunnel: true,          // Instruksi: jalankan tunnel
-    cftoken: "eyJhIjoiMmRhODIzODc4MTYwMzQzNWY2MzllMDgxMjQ3NGVmZWYiLCJ0IjoiZDk2NjYxODUtNjI3NC00OWExLWIzNWUtYTExZWQzOWRmMTU2IiwicyI6Ik56VTRNV1E1WVdZdFpqTTFaQzAwTjJZekxUZzFabU10TmpCa01EQTBNMlkyWXpZeCJ9",    // Data: token Cloudflare
+    cftoken: "eyJhIjoiMmRhODIzODc4MTYwMzQzNWY2MzllMDgxMjQ3NGVmZWYiLCJ0IjoiZTBmMGYwNWUtMWI4ZC00N2NhLWEyMGUtYjVhMzFkYTE1OWRhIiwicyI6IlptUTNPVEU0Tm1RdE5XVmxOeTAwT0dVMkxXRmpPR1V0TkRFME1UVXpNMlZpWmpReSJ9",    // Data: token Cloudflare
     secretkey: "xixy-panel-super-lite",
     port: 8000
 };
